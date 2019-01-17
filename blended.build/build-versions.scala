0d6312d004a747ed78aa27c8b53441e02449ed46
Andreas Gies <andreas@wayofquality.de>
2019-01-17 08:44:30 +0100

    After releasing 3.0-M3

--- blended.build/build-versions.scala
+++ blended.build/build-versions.scala
@@ -1,7 +1,7 @@
 object BlendedVersions {
 
   val blendedGroupId = "de.wayofquality.blended"
-  val blendedVersion = "3.0-M3"
+  val blendedVersion = "3.0-M4-SNAPSHOT"
 
   val javaVersion = "1.8"
 
