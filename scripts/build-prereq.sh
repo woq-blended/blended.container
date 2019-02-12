88075028178749573a917e9c3e66029bdea0f5dc
Tobias Roeser <le.petit.fou@web.de>
2019-02-12 10:53:56 +0100

    Added prerequisites to docker build

---
+++ scripts/build-prereq.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -e
+
+publishLocal() {
+  cd "${TRAVIS_BUILD_DIR}"
+  local REPO="https://github.com/woq-blended/$1.git"
+  local REPODIR="${TRAVIS_BUILD_DIR}/$1"
+  rm -Rf ${REPODIR}
+  echo "Cloning ${REPO}..."
+  git clone --branch master --depth 1 \
+    "$REPO" "$REPODIR"
+  cd "$REPODIR"
+  echo "Building and (local) publishing ${REPO}..."
+  sbt publishLocal
+  cd "${TRAVIS_BUILD_DIR}"
+}
+
+publishLocal "$@"
