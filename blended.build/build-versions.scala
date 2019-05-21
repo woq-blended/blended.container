ecfb47308be742c5c4562340b6a5640082109f7b
Andreas Gies <andreas@wayofquality.de>
2019-05-21 18:48:47 +0200

    Releasing 3.0-M13

--- blended.build/build-versions.scala
+++ blended.build/build-versions.scala
@@ -1,7 +1,7 @@
 object BlendedVersions {
 
   val blendedGroupId = "de.wayofquality.blended"
-  val blendedVersion = "3.0-M13-SNAPSHOT"
+  val blendedVersion = "3.0-M13"
 
   val javaVersion = "1.8"
 
