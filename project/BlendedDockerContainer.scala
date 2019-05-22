d61868db496267938e754c93926a9dc1106cb937
Tobias Roeser <le.petit.fou@web.de>
2019-05-22 11:00:17 +0200

    Use phonix docker abstraction from unreleased sbt-blendedbuild plugin

--- project/BlendedDockerContainer.scala
+++ project/BlendedDockerContainer.scala
@@ -1,55 +1,17 @@
-import java.io.File
-
-import scala.sys.process.Process
-
-import com.typesafe.sbt.packager.universal.{UniversalDeployPlugin, UniversalPlugin}
-import de.wayofquality.sbt.filterresources.FilterResources
 import sbt.Keys._
 import sbt._
-import blended.sbt.dockercontainer.BlendedDockerContainerPlugin
-import blended.sbt.dockercontainer.BlendedDockerContainerPlugin.{autoImport => DC}
-import blended.sbt.container.BlendedContainerPlugin.{autoImport => BC}
+import blended.sbt.phoenix.docker.DockerContainer
 
-trait BlendedDockerContainer extends ProjectSettings {
+trait BlendedDockerContainer extends ProjectSettings with DockerContainer {
 
-  ///////
+  override def blendedVersion = Blended.blendedVersion
 
-  def imageTag: String
-
-  def folder: String
-
-  def ports: List[Int] = List()
-
-  /**
-   * Filenames to overlays, relative to the project directory.
-   */
-  def overlays: Seq[String] = Seq()
-
-  def env: Map[String, String] = Map()
-
-  def profileName: Option[String] = None
-
-  def maintainer: String = s"Blended Team version: ${Blended.blendedVersion}"
-
-  override def plugins: Seq[AutoPlugin] = super.plugins ++ Seq(
-    BlendedDockerContainerPlugin
-  )
-
-  //////
+  override def maintainer: String = s"Blended Team version: ${Blended.blendedVersion}"
 
   override def settings: Seq[sbt.Setting[_]] = super.settings ++ Seq(
     Compile / packageBin / publishArtifact := false,
     Compile / packageDoc / publishArtifact := false,
-    Compile / packageSrc / publishArtifact := false,
-
-    BC.blendedVersion := Blended.blendedVersion,
-    DC.appFolder := folder,
-    DC.imageTag := imageTag,
-    DC.ports := ports,
-    DC.overlays := overlays.map(o => target.value / o),
-    DC.env := env,
-    DC.profile := profileName.map(n => n -> version.value),
-    DC.maintainer := maintainer
+    Compile / packageSrc / publishArtifact := false
   )
 
   def containerDep: Option[ModuleID] = None
