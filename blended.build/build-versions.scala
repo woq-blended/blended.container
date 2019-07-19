b9f9a3895636b9881dd519c4e5de466bd9cdded2
Andreas Gies <andreas@wayofquality.de>
2019-07-19 12:43:37 +0200

    Releasing 3.0-M19

--- blended.build/build-versions.scala
+++ blended.build/build-versions.scala
@@ -1,7 +1,7 @@
 object BlendedVersions {
 
   val blendedGroupId = "de.wayofquality.blended"
-  val blendedVersion = "3.0-M19-SNAPSHOT"
+  val blendedVersion = "3.0-M19"
 
   val javaVersion = "1.8"
 
