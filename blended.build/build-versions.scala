2584c44c1fa2740586218226052828298170cbb1
Andreas Gies <andreas@wayofquality.de>
2020-01-15 11:10:39 +0100

    Releasing 3.0.13

--- blended.build/build-versions.scala
+++ blended.build/build-versions.scala
@@ -1,7 +1,7 @@
 object BlendedVersions {
 
   val blendedGroupId = "de.wayofquality.blended"
-  val blendedVersion = "3.0.13-SNAPSHOT"
+  val blendedVersion = "3.0.13"
 
   val javaVersion = "1.8"
 
